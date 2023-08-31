2bc24a8c3278c060130d822a6bd24a04ae6e12b0
Cédric Sazos <cedric.sazos@tutanota.com>
2023-08-31 15:40:51 +0200

    Renamed river to flop

--- src/GameState.cs
+++ src/GameState.cs
@@ -9,14 +9,14 @@ public struct GameState
     public readonly int Turn;
     public readonly Player Player;
     public IReadOnlyList<int> Bets;
-    public IReadOnlyList<Card> River;
+    public IReadOnlyList<Card> Flop;
 
 
-    public GameState(int turn, Player player, List<int> bets, List<Card> river) {
+    public GameState(int turn, Player player, List<int> bets, List<Card> flop) {
         Turn = turn;
         Player = player;
         Bets = bets;
-        River = river;
+        Flop = flop;
     }
 
 }
