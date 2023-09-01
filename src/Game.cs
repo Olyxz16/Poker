ea4c029fa791ca2006505f8eb1503d2f2c56fc5e
Cédric Sazos <cedric.sazos@tutanota.com>
2023-09-01 21:34:06 +0200

    v1

--- src/Game.cs
+++ src/Game.cs
@@ -1,3 +1,4 @@
+using Poker.Cards;
 using Poker.Players;
 
 namespace Poker;
@@ -7,10 +8,95 @@ public class Game
 
     public readonly static int SMALL_BLIND = 10;
     public static int BIG_BLIND => 2*SMALL_BLIND;
-    
-    //private List<Player> _players;
-    //public IReadOnlyList<Player> Players { get => _players; }
 
-    //public Board Board { get; private set; }
+    
+    private List<Player> _players;
+    public IReadOnlyList<Player> Players { get => _players; }
+
+    private List<int> _bets;
+    public IReadOnlyList<int> Bets { get => _bets; }
+
+    private List<Card> _flop;
+    public IReadOnlyList<Card> River { get => _flop; }
+
+    private Deck _deck;
+    private int _bank;
+
+
+    public Game(List<Player> players) {
+
+        _players = players;
+        _bets = new List<int>();
+        _flop = new List<Card>();
+
+        _deck = Deck.Standard52CardDeck();
+        _deck.Shuffle();
+
+        _bank = 0;
+
+    }
+
+    public void Run() {
+        while(Players.Count > 1) {
+            var winner = PlayGame();
+            RemoveLosers();
+        }
+    }
+
+    private Player PlayGame() {
+        InitGame();
+        var remainingPlayers = new List<Player>(_players);
+        for(int round = 1 ; round < 3 ; round++) {
+            PlayRound(remainingPlayers);
+        }
+        var winner = ComputeWinner();
+        winner.Balance += _bank;
+        return winner;
+    }
+
+    private void InitGame() {
+        _bank = 0;
+        _flop = _deck.Draw(5);
+        foreach(var player in _players) {
+            player.DrawHand(_deck.Draw(2));
+        }
+    }
+
+    private void PlayRound(List<Player> remainingPlayers) {
+        for(int i = 0 ; i < remainingPlayers.Count ; i++) {
+            var player = remainingPlayers[i];
+            var turn = i+1;
+            var gameState = GetGameState(turn, player);
+            var move = player.Play(gameState);
+            if(move.MoveType == MoveType.FOLD) {
+                remainingPlayers.Remove(player);
+                i--;
+            } else if(move.MoveType == MoveType.BET) {
+                _bank += move.BetValue;
+            }
+        }
+    }
+
+    public GameState GetGameState(int turn, Player player) {
+        return new GameState(turn, player, _bets, _flop);
+    }
+
+
+    public void RemoveLosers() {
+        for(int i = _players.Count-1 ; i >= 0; i--) {
+            if(_players[i].Balance <= 0) {
+                _players.RemoveAt(i);
+            }
+        }
+    }
+
+    private Player ComputeWinner() {
+        var winner = _players[0];
+        for(int i = 0 ; i < _players.Count ; i++) {
+            winner = Combination.Compare(winner, _players[i], _flop);
+        }
+        return winner; 
+    }
+
 
 }
---
+++ src/cards/Combination.cs
@@ -0,0 +1,123 @@
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+using Poker.Players;
+
+namespace Poker.Cards;
+
+public struct Combination
+{
+
+    private int _flag;
+
+
+    private Combination(Player player, List<Card> flop) {
+        _flag = 0;
+        var cards = OrderCards(player, flop);
+        int pairs = NumberOfPairs(cards);
+        _flag |= IsFlush(cards) ? (int)CombinationType.FLUSH: 0;
+        _flag |= IsStraight(cards) ? (int)CombinationType.STRAIGHT: 0;
+        _flag |= IsFOAK(cards) ? (int)CombinationType.FOUR_OF_A_KIND: 0;
+        _flag |= IsTOAK(cards) ? (int)CombinationType.THREE_OF_A_KIND : 0;
+        _flag |= pairs == 2 ? (int)CombinationType.TWO_PAIR : 0;
+        _flag |= pairs == 1 ? (int)CombinationType.PAIR : 0;
+
+        _flag |= _flag == 0 ? (int)CombinationType.HIGH_CARD : 0;
+
+        int isFullHouseFlag = (int)CombinationType.THREE_OF_A_KIND | (int)CombinationType.PAIR;
+        _flag = (_flag & isFullHouseFlag) == isFullHouseFlag ? (int)CombinationType.FULL_HOUSE : 0;
+        
+        int isStraightFlushFlag = (int)CombinationType.STRAIGHT | (int)CombinationType.FLUSH;
+        _flag |= (_flag & isStraightFlushFlag) == isStraightFlushFlag ? (int)CombinationType.STRAIGHT_FLUSH : 0;
+        
+        _flag |= (_flag & (int)CombinationType.STRAIGHT_FLUSH) != 0 && cards[0].Rank == 10 ? (int)CombinationType.ROYAL_FLUSH : 0;
+    }
+
+
+    public static Player Compare(Player p1, Player p2, List<Card> flop) {
+        var c1 = new Combination(p1, flop);
+        var c2 = new Combination(p2, flop);
+
+        if(c1._flag == c2._flag) {
+            return CompareHands(p1, p2);
+        } else {
+            return c1._flag > c2._flag ? p1 : p2;
+        }
+    }
+
+    private static Player CompareHands(Player p1, Player p2) {
+        static int cmp(Card card) => card.Rank;
+        var p1max = p1.Hand.Max(cmp);
+        var p2max = p2.Hand.Max(cmp);
+        var p1min = p1.Hand.Min(cmp);
+        var p2min = p2.Hand.Min(cmp);
+        if (p1max == p2max) {
+            return p1min > p2min ? p1 : p2;
+        }
+        return p1min > p2min ? p1 : p2;
+    }
+
+    private static List<Card> OrderCards(Player player, List<Card> flop) {
+        var result = new List<Card>(flop);
+        result.AddRange(player.Hand);
+        result = new List<Card>(result.OrderBy(card => card.Rank));
+        return result;
+    }
+
+    private static bool IsFlush(List<Card> cards) {
+        var color = cards[0].Color;
+        return cards.All(card => card.Color == color);
+    }
+    private static bool IsStraight(List<Card> cards) {
+        int start = cards[0].Rank;
+        bool hasAce = start == 1;
+        int following = 1;
+        for(int i = 1 ; i < 5 ; i++) {
+            if(cards[i].Rank != start + i) {
+                break;
+            }
+            following++;
+        }
+        return following == 5 || (start == 10 && hasAce);
+    }
+    private static bool IsFOAK(List<Card> cards) {
+        foreach(var card in cards) {
+            if(cards.Select(c => c.Rank == card.Rank).Count() == 4) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static bool IsTOAK(List<Card> cards) {
+        foreach(var card in cards) {
+            if(cards.Select(c => c.Rank == card.Rank).Count() == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static int NumberOfPairs(List<Card> cards) {
+        int pairs = 0;
+        foreach(var card in cards) {
+            if(cards.Select(c => c.Rank == card.Rank).Count() == 2) {
+                pairs++;
+            }
+        }
+        return pairs/2;
+    }
+
+}
+
+[Flags]
+public enum CombinationType {
+    ROYAL_FLUSH =     0b_1000000000,  
+    STRAIGHT_FLUSH =  0b_0100000000,    
+    FOUR_OF_A_KIND =  0b_0010000000,
+    FULL_HOUSE =      0b_0001000000,
+    FLUSH =           0b_0000100000,
+    STRAIGHT =        0b_0000010000,
+    THREE_OF_A_KIND = 0b_0000001000,
+    TWO_PAIR =        0b_0000000100,
+    PAIR =            0b_0000000010,
+    HIGH_CARD =       0b_0000000001
+}
