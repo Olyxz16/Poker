df06241961aeb9baf5c206432bac7705a03bd0ba
Cédric Sazos <cedric.sazos@tutanota.com>
2023-09-07 13:07:16 +0200

    [test] Added new combination tests

--- test/CombinationTest.cs
+++ test/CombinationTest.cs
@@ -69,7 +69,79 @@ public class CombinationTest
         var p1 = new TestPlayer(100);
         p1.DrawHand(new(1, CardColor.CLUB), new(10, CardColor.SPADE));
         var p2 = new TestPlayer(100);
-        p2.DrawHand(new(1, CardColor.HEART), new(4, CardColor.SPADE));
+        p2.DrawHand(new(1, CardColor.HEART), new(9, CardColor.SPADE));
+        var winner = Combination.Compare(p1, p2, flop);
+        Assert.Same(winner, p1);
+    }
+
+
+    /// <summary>
+    /// PAIR_TESTS
+    /// </summary>
+    
+    [Fact]
+    public void Pair_Neutral_P1() {
+        var flop = new List<Card>() {
+            new(10, CardColor.CLUB),
+            new(7, CardColor.DIAMOND),
+            new(2, CardColor.CLUB),
+            new(11, CardColor.HEART),
+            new(12, CardColor.SPADE)   
+        };
+        var p1 = new TestPlayer(100);
+        p1.DrawHand(new(10, CardColor.CLUB), new(6, CardColor.SPADE));
+        var p2 = new TestPlayer(100);
+        p2.DrawHand(new(13, CardColor.HEART), new(3, CardColor.SPADE));
+        var winner = Combination.Compare(p1, p2, flop);
+        Assert.Same(winner, p1);
+    }
+    [Fact]
+    public void Pair_Neutral_P2() {
+        var flop = new List<Card>() {
+            new(10, CardColor.CLUB),
+            new(7, CardColor.DIAMOND),
+            new(2, CardColor.CLUB),
+            new(11, CardColor.HEART),
+            new(12, CardColor.SPADE)   
+        };
+        var p1 = new TestPlayer(100);
+        p1.DrawHand(new(9, CardColor.CLUB), new(6, CardColor.SPADE));
+        var p2 = new TestPlayer(100);
+        p2.DrawHand(new(13, CardColor.HEART), new(2, CardColor.SPADE));
+        var winner = Combination.Compare(p1, p2, flop);
+        Assert.Same(winner, p2);
+    }
+
+    [Fact]
+    public void Pair_Equal_Compare_High_Card() {
+        var flop = new List<Card>() {
+            new(10, CardColor.CLUB),
+            new(7, CardColor.DIAMOND),
+            new(2, CardColor.CLUB),
+            new(11, CardColor.HEART),
+            new(12, CardColor.SPADE)   
+        };
+        var p1 = new TestPlayer(100);
+        p1.DrawHand(new(2, CardColor.CLUB), new(6, CardColor.SPADE));
+        var p2 = new TestPlayer(100);
+        p2.DrawHand(new(13, CardColor.HEART), new(2, CardColor.SPADE));
+        var winner = Combination.Compare(p1, p2, flop);
+        Assert.Same(winner, p2);
+    }
+
+    [Fact]
+    public void Pair_Compare_Different() {
+        var flop = new List<Card>() {
+            new(10, CardColor.CLUB),
+            new(7, CardColor.DIAMOND),
+            new(2, CardColor.CLUB),
+            new(11, CardColor.HEART),
+            new(12, CardColor.SPADE)   
+        };
+        var p1 = new TestPlayer(100);
+        p1.DrawHand(new(10, CardColor.CLUB), new(6, CardColor.SPADE));
+        var p2 = new TestPlayer(100);
+        p2.DrawHand(new(13, CardColor.HEART), new(2, CardColor.SPADE));
         var winner = Combination.Compare(p1, p2, flop);
         Assert.Same(winner, p1);
     }
