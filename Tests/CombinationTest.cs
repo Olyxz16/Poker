c35fc7653be9198b5acd26d6293416dda7682d6f
Cédric Sazos <cedric.sazos@tutanota.com>
2023-09-24 19:13:49 +0200

    added full house tests

--- Tests/CombinationTest.cs
+++ Tests/CombinationTest.cs
@@ -389,4 +389,51 @@ public class CombinationTest
 
 
 
+
+    /// <summary>
+    /// Full_House_TESTS
+    /// </summary>
+
+    [Fact]
+    public void Full_House_Neutral1() {
+        var flop = new List<Card>() {
+            new(5, CardColor.HEART),
+            new(5, CardColor.CLUB),
+            new(9, CardColor.CLUB),
+            new(11, CardColor.HEART),
+            new(13, CardColor.SPADE)   
+        };
+        var p1 = new TestPlayer(100);
+        p1.DrawHand(new(5, CardColor.CLUB), new(9, CardColor.CLUB));
+        var p2 = new TestPlayer(100);
+        p2.DrawHand(new(13, CardColor.HEART), new(3, CardColor.SPADE));
+        var c1 = new Combination(p1, flop);
+        Assert.Equal(CombinationType.FULL_HOUSE, c1.GetCombinationType());
+        var winner = Combination.Compare(p1, p2, flop);
+        Assert.Same(winner, p1);
+    }
+
+    [Fact]
+    public void Full_House_Neutral2() {
+        var flop = new List<Card>() {
+            new(5, CardColor.CLUB),
+            new(5, CardColor.HEART),
+            new(5, CardColor.CLUB),
+            new(4, CardColor.HEART),
+            new(13, CardColor.HEART)   
+        };
+        var p1 = new TestPlayer(100);
+        p1.DrawHand(new(13, CardColor.CLUB), new(3, CardColor.SPADE));
+        var p2 = new TestPlayer(100);
+        p2.DrawHand(new(13, CardColor.HEART), new(4, CardColor.HEART));
+        var c2 = new Combination(p2, flop);
+        Assert.Equal(CombinationType.FULL_HOUSE, c2.GetCombinationType());
+        var winner = Combination.Compare(p1, p2, flop);
+        Assert.Same(winner, p2);
+    }
+
+
+    
+
+
 }
