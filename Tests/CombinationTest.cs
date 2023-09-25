c6d86243b92b72c4a897aff40a0a4ce7eac21ba2
Cédric Sazos <cedric.sazos@tutanota.com>
2023-09-25 20:46:49 +0200

    Added straight flush tests

--- Tests/CombinationTest.cs
+++ Tests/CombinationTest.cs
@@ -495,6 +495,50 @@ public class CombinationTest
         var winner = Combination.Compare(p1, p2, flop);
         Assert.Same(winner, p2);
     }
+
+
+
+    /// <summary>
+    /// Straight_flush_TESTS
+    /// </summary>
+
+    [Fact]
+    public void Straight_flush_Neutral1() {
+        var flop = new List<Card>() {
+            new(2, CardColor.CLUB),
+            new(5, CardColor.CLUB),
+            new(9, CardColor.HEART),
+            new(6, CardColor.CLUB),
+            new(13, CardColor.SPADE)   
+        };
+        var p1 = new TestPlayer(100);
+        p1.DrawHand(new(4, CardColor.CLUB), new(3, CardColor.CLUB));
+        var p2 = new TestPlayer(100);
+        p2.DrawHand(new(13, CardColor.HEART), new(3, CardColor.SPADE));
+        var c1 = new Combination(p1, flop);
+        Assert.Equal(CombinationType.STRAIGHT_FLUSH, c1.GetCombinationType());
+        var winner = Combination.Compare(p1, p2, flop);
+        Assert.Same(winner, p1);
+    }
+
+    [Fact]
+    public void Straight_flush_Neutral2() {
+        var flop = new List<Card>() {
+            new(8, CardColor.CLUB),
+            new(2, CardColor.HEART),
+            new(5, CardColor.CLUB),
+            new(4, CardColor.HEART),
+            new(3, CardColor.HEART)   
+        };
+        var p1 = new TestPlayer(100);
+        p1.DrawHand(new(13, CardColor.CLUB), new(3, CardColor.SPADE));
+        var p2 = new TestPlayer(100);
+        p2.DrawHand(new(1, CardColor.HEART), new(5, CardColor.HEART));
+        var c2 = new Combination(p2, flop);
+        Assert.Equal(CombinationType.STRAIGHT_FLUSH, c2.GetCombinationType());
+        var winner = Combination.Compare(p1, p2, flop);
+        Assert.Same(winner, p2);
+    }
     
 
 
