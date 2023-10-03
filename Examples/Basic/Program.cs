4df8d0a4b8c4e6d700f3700d1d11eaef7f13585d
Cédric Sazos <cedric.sazos@tutanota.com>
2023-10-03 16:44:34 +0200

    Update example

--- Examples/Basic/Program.cs
+++ Examples/Basic/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Poker;
+﻿using Poker;
+using Poker.Cards;
+using Poker.Players;
+
+namespace Basic;
 
 public class Program
 {
     public static void Main(string[] args) 
     {
-
+        var p1 = new ConsolePlayer(100);
+        var p2 = new ConsolePlayer(100);
+        var game = new Game(new List<Player> {p1,p2});
+        game.Play();
     }
 }
