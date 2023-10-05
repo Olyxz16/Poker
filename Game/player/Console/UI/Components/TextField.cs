3ee776e7d907770813036ca259146ce7eef7ce62
Cédric Sazos <cedric.sazos@tutanota.com>
2023-10-05 16:42:46 +0200

    Added setText to textfield

--- Game/player/Console/UI/Components/TextField.cs
+++ Game/player/Console/UI/Components/TextField.cs
@@ -1,11 +1,17 @@
-
-
 namespace GUISharp.Components;
 
 public class TextField : Component {
 
     public TextField(string text) : base(text.Length, 1) {
         for(int i = 0 ; i < text.Length ; i++) {
+            _content[i,0] = text[i];
+        }
+    }
+
+    public void SetText(string text) {
+        int len = text.Length;
+        _content = new char[len,1];
+        for(int i = 0 ; i < len ; i++) {
             _content[i,0] = text[i];
         }
     }
