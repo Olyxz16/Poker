339c3e0c4f7a2495c0e221e48fdc7ce8c67d7983
Cédric Sazos <cedric.sazos@tutanota.com>
2023-10-06 22:00:49 +0200

    Added depth to TextField

--- Game/player/Console/UI/Components/TextField.cs
+++ Game/player/Console/UI/Components/TextField.cs
@@ -2,7 +2,7 @@ namespace GUISharp.Components;
 
 public class TextField : Component {
 
-    public TextField(string text) : base(text.Length, 1) {
+    public TextField(string text, int depth = 0) : base(text.Length, 1, depth) {
         for(int i = 0 ; i < text.Length ; i++) {
             _content[i,0] = text[i];
         }
