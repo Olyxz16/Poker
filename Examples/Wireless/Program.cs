103f0ce57db8d9a43f665de1e4878f97d64a3e3f
Cédric Sazos <cedric.sazos@tutanota.com>
2023-10-17 22:31:03 +0200

    Updated template

--- Examples/Wireless/Program.cs
+++ Examples/Wireless/Program.cs
@@ -1,16 +1,19 @@
-﻿
-
-
-using Poker.Players.Net;
+﻿using Poker.Players.Net;
 
 public class Program
 {
     public static void Main(string[] args) 
     {
         
-
         var server = Server.Instance;
+        var client = new Client();
 
+        var player = server._players.First().Key;
+        var req = new Protocol()
+            .SetValue("test", 0); 
+
+        server.SendAndWaitForAnswer(player, req);
+        while(server.IsRunning);
 
     }
 }
