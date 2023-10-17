af4c17c6fd6ff89389e04a4e7c1665f4ea83a54f
Cédric Sazos <cedric.sazos@tutanota.com>
2023-10-17 22:30:24 +0200

    Created Protocol to communicate between server and client

---
+++ Game/player/Console/Net/Protocol.cs
@@ -0,0 +1,43 @@
+using System.Text.Json;
+
+namespace Poker.Players.Net;
+
+public class Protocol
+{
+    
+    public IReadOnlyList<object> Keys => _values.Keys.ToList();
+
+    private Dictionary<string, object> _values;
+    
+
+
+    public Protocol() {
+        _values = new Dictionary<string, object>();
+    }
+    private Protocol(Dictionary<string, object> from) {
+        _values = from;
+    }
+
+
+    public Protocol SetValue(string key, object value) {
+        _values[key] = value;
+        return this;
+    }
+
+    public int GetInt(string key) {
+        return ((JsonElement)_values[key]).GetInt32();
+    }
+
+
+    public string Serialize() {
+        return JsonSerializer.Serialize(_values);
+    }
+    public static Protocol Parse(string value) {
+        Dictionary<string, object> result = 
+            JsonSerializer.Deserialize<Dictionary<string, object>>(value)
+            ?? new Dictionary<string, object>();
+        return new Protocol(result);
+    }
+
+
+}
