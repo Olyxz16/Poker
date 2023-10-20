3f6c81df85d82a0369c0a68cf319331cc9f7a8b0
Cédric Sazos <cedric.sazos@tutanota.com>
2023-10-20 22:37:34 +0200

    Updated sample project

--- Examples/Wireless/Program.cs
+++ Examples/Wireless/Program.cs
@@ -1,19 +1,27 @@
-﻿using Poker.Players.Net;
+﻿using System.Runtime.CompilerServices;
+using Poker;
+using Poker.Players;
+using Poker.Players.Net;
 
 public class Program
 {
+
     public static void Main(string[] args) 
     {
         
+        var players = new List<Player>();
+
         var server = Server.Instance;
+        server.OnPlayerAddEvent += AddAndClear;
         var client = new Client();
 
-        var player = server._players.First().Key;
-        var req = new Protocol()
-            .SetValue("test", 0); 
-
-        server.SendAndWaitForAnswer(player, req);
         while(server.IsRunning);
 
+        void AddAndClear(Player player) {
+            players.Add(player);
+            (player as RemoteConsolePlayer).Clear();
+        }
+
     }
+
 }
