99afa54d7eaaa7be51d523a724f5087cf1aac907
Cédric Sazos <cedric.sazos@tutanota.com>
2023-10-21 17:25:40 +0200

    Added Lobby to wait for players before the game

---
+++ Game/Lobby.cs
@@ -0,0 +1,40 @@
+using Poker.Players;
+using Poker.Players.Net;
+
+namespace Poker;
+
+public class Lobby
+{
+    
+    private readonly int _balance;
+    private readonly List<Player> _players;
+    private readonly Server server;
+
+    public Lobby() : this(Game.DEFAULT_BALANCE) {}
+    public Lobby(int balance) {
+        _balance = balance;
+        _players = new List<Player>();
+        server = Server.Instance;
+        server.OnPlayerAddEvent += AddPlayer;
+    }
+
+    public List<Player> WaitForPlayers() {
+        var key = Console.ReadKey();
+        while(key.Key != ConsoleKey.Enter) {
+            Console.WriteLine("Press Enter to stop.");
+            key = Console.ReadKey();
+        }
+        return _players;
+    }
+
+
+
+    private void AddPlayer(Player player) {
+        _players.Add(player);
+        Console.WriteLine("Player added");
+    }
+
+
+    
+
+}
