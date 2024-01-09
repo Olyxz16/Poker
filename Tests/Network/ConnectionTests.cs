ec701baea9e792bdbe70c7eac11f62712f099a5c
Cédric Sazos <cedric.sazos@tutanota.com>
2024-01-09 13:39:50 +0100

    created networking connection test

---
+++ Tests/Network/ConnectionTests.cs
@@ -0,0 +1,22 @@
+using Poker.Players;
+using Poker.Players.Net;
+
+namespace Tests;
+
+public class ConnectionTest 
+{
+
+    [Fact]
+    public void ConnectLocal() {
+        bool connected = false;
+        var server = Server.Instance;
+        server.OnPlayerAddEvent += (Player p) => {
+            connected = true;
+        };
+        var client = new Client("127.0.0.1", 8080);
+
+        while(!connected);
+        Assert.Single(server.Clients);
+    }
+
+}
