9ba1bbef64ad45822c1769c94a800e6aab57b7e2
Cédric Sazos <cedric.sazos@tutanota.com>
2024-04-23 20:07:53 +0200

    Fixed stuff in WeightUtils

--- Src/Player/IA/Utils/WeightUtils.cs
+++ Src/Player/IA/Utils/WeightUtils.cs
@@ -6,11 +6,44 @@ namespace Poker.Players.IA.Utils;
 
 public static class WeightUtils {
     
+
+    public static NDarray InputFromState(GameState state) {
+        var player = state.Player;
+        var balance = player.Balance;
+        var flop = state.Flop;
+        var maxPot = state.Bets.Count > 0 ? state.Bets.Values.Max() : 0;
+
+        var inputs = new List<double>();
+        var playerCard1 = WeightsFromCard(player.Hand[0]);
+        var playerCard2 = WeightsFromCard(player.Hand[1]);
+        var flopCard1 = WeightsFromCard(flop.ElementAtOrDefault(0));
+        var flopCard2 = WeightsFromCard(flop.ElementAtOrDefault(1));
+        var flopCard3 = WeightsFromCard(flop.ElementAtOrDefault(2));
+        var flopCard4 = WeightsFromCard(flop.ElementAtOrDefault(3));
+        var flopCard5 = WeightsFromCard(flop.ElementAtOrDefault(4));
+        // To avoid division by zero but janky
+        var maxPotVal = player.Balance > 0 ? Math.Min(maxPot / player.Balance, 1) : 1;
+        var balanceVal = WeightFromBalance(balance);
+
+        inputs.AddRange(playerCard1);
+        inputs.AddRange(playerCard2);
+        inputs.AddRange(flopCard1);
+        inputs.AddRange(flopCard2);
+        inputs.AddRange(flopCard3);
+        inputs.AddRange(flopCard4);
+        inputs.AddRange(flopCard5);
+        inputs.Add(maxPotVal);
+        inputs.Add(balanceVal);
+        
+        return np.array(inputs.ToArray());
+    }
+
+
     // Move
     public static Move MoveFromOutput(NDarray output, GameState state) {
         var ind = MaxInd(output);
-        var maxBet = state.Bets.Values.Max();
-        return ind switch {
+        var maxBet = state.Bets.Count() == 0 ? 0: state.Bets.Values.Max();
+        var move = ind switch {
             0 => Move.Fold(),
             1 => Move.Bet(0),
             2 => Move.Bet(maxBet),
@@ -18,6 +51,18 @@ public static class WeightUtils {
             3 => Move.Bet(BalanceFromWeight(output.item<double>(4))),
             _ => Move.Fold()
         };
+        // failsafe
+        var bal = state.Player.Balance;
+        if(state.Turn == 1) {
+            return Move.Bet(Math.Min(Game.SMALL_BLIND, bal));
+        }
+        if(state.Turn == 2) {
+            return Move.Bet(Math.Min(Game.BIG_BLIND, bal));
+        }
+        if(move.BetValue > bal) {
+            return Move.Bet(bal);
+        }
+        return move;
     }
 
     private static int MaxInd(NDarray array) {
