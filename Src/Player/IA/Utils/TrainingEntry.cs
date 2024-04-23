f5d21fcc18d43bc3bf2e1307865cb7c9bd2ed1e4
Cédric Sazos <cedric.sazos@tutanota.com>
2024-04-23 20:08:18 +0200

    Reworked TrainingUtils

--- Src/Player/IA/Utils/TrainingEntry.cs
+++ Src/Player/IA/Utils/TrainingEntry.cs
@@ -1,12 +1,14 @@
+using Numpy;
+
 namespace Poker.Players.IA.Utils;
 
 public struct TrainingEntry {
     
-    public readonly double[] inputs;
-    public readonly double[] outputs;
+    public readonly NDarray inputs;
+    public readonly NDarray outputs;
     public readonly double reward;
 
-    public TrainingEntry(double[] inputs, double[] outputs, double reward) {
+    public TrainingEntry(NDarray inputs, NDarray outputs, double reward) {
         this.inputs = inputs;
         this.outputs = outputs;
         this.reward = reward;
--- Src/Player/IA/Utils/TrainingUtils.cs
+++ Src/Player/IA/Utils/TrainingUtils.cs
@@ -1,4 +1,5 @@
 using Keras.Models;
+using Numpy;
 using Poker.Events;
 
 namespace Poker.Players.IA.Utils;
@@ -6,7 +7,7 @@ namespace Poker.Players.IA.Utils;
 
 public class TrainingUtils {
 
-    private readonly List<(GameState, Move)> _entries = new();
+    private readonly List<(Player, NDarray, NDarray)> _entries = new();
     private readonly List<TrainingEntry> _trainingData = new();
 
 
@@ -14,13 +15,20 @@ public class TrainingUtils {
         _entries = new();
         _trainingData = new();
 
-        Game.PlayerMoveEvent += AddEntry;
+        Game.GameStartEvent += AddPlayerEvents;
         Game.GameEndEvent += CompileResults;
     }
 
-
+    public void AddPlayerEvents(object sender, GameStartEventArgs e) {
+        var players = e.Players;
+        foreach(var p in players) {
+            if(p is NeuralNetPlayer neuralPlayer) {
+                neuralPlayer.OnPlayerMoveEvent += AddEntry;
+            }
+        }
+    }
     public void AddEntry(object sender, PlayerMoveEventArgs e) {
-        _entries.Add((e.State, e.Move));
+        _entries.Add((e.Player, e.Inputs, e.Outputs));
     }
 
     public void LoadData(string path) {
@@ -29,7 +37,7 @@ public class TrainingUtils {
     }
 
 
-    public void Train(Model model, int epochs) {
+    public void Train(Sequential model, int epochs) {
         for (int i = 0; i < epochs; i++)
         {
             // Iterate over the training data
@@ -41,7 +49,7 @@ public class TrainingUtils {
 
                 var targetValue = reward > 0 ? 1 : -1;
 
-                model.TrainOnBatch(new[] { state }, new[] { action }, new[] { targetValue });
+                model.TrainOnBatch(state, action , new[] { targetValue });
             }
         }
     }
@@ -49,57 +57,10 @@ public class TrainingUtils {
 
     private void CompileResults(object sender, GameEndEventArgs e) {
         var winner = e.State.Winner;
-        foreach(var (state, move) in _entries) {
-            var data = Compile(state, move, winner);
+        foreach(var (player, inputs, outputs) in _entries) {
+            var reward = player == winner ? 1d : 0d;
+            var data = new TrainingEntry(inputs, outputs, reward);
             _trainingData.Add(data);
-        }
-    }
-    private TrainingEntry Compile(GameState state, Move move, Player winner) {
-        var inputs = CompileInputs(state);
-        var outputs = CompileOutputs(move, state.Player.Balance);
-        var reward = state.Player == winner ? 1d : 0d;
-        return new TrainingEntry(inputs, outputs, reward);
-    }
-
-    private double[] CompileInputs(GameState state) {
-        var player = state.Player;
-        var balance = player.Balance;
-        var flop = state.Flop;
-        var maxPot = state.Bets.Max().Value;
-
-        var inputs = new List<double>();
-        var playerCard1 = WeightUtils.WeightsFromCard(player.Hand[0]);
-        var playerCard2 = WeightUtils.WeightsFromCard(player.Hand[1]);
-        var flopCard1 = WeightUtils.WeightsFromCard(flop[0]);
-        var flopCard2 = WeightUtils.WeightsFromCard(flop[1]);
-        var flopCard3 = WeightUtils.WeightsFromCard(flop[2]);
-        var flopCard4 = WeightUtils.WeightsFromCard(flop[3]);
-        var flopCard5 = WeightUtils.WeightsFromCard(flop[4]);
-        var maxPotVal = Math.Min(maxPot / player.Balance, 1);
-        var balanceVal = WeightUtils.WeightFromBalance(balance);
-
-        inputs.AddRange(playerCard1);
-        inputs.AddRange(playerCard2);
-        inputs.AddRange(flopCard1);
-        inputs.AddRange(flopCard2);
-        inputs.AddRange(flopCard3);
-        inputs.AddRange(flopCard4);
-        inputs.AddRange(flopCard5);
-        inputs.Add(maxPotVal);
-        inputs.Add(balanceVal);
-
-        return inputs.ToArray();
-    }
-    // WARNING : NOT THE ACTUAL DATA, JUST A RECONSTRUCTION. WOULD NEED A WAY TO PULL DATA FROM MODEL WHEN CONSTRUCTION MOVE.
-    private double[] CompileOutputs(Move move, int balance) {
-        if(move.MoveType == MoveType.FOLD) {
-            return new[] {1d, 0d, 0d, 0d};
-        } else if(move.BetValue > 0) {
-            var value = WeightUtils.WeightFromBalance(balance);
-            return new[] {0d, 0d, 1d, value};
-        }
-        else {
-            return new[] {0d, 1d, 0d, 0d};
         }
     }
 
