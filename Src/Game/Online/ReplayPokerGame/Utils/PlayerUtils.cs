958f0797c8a93bf05deaa87240421886f994049f
Cédric Sazos <cedric.sazos@tutanota.com>
2024-05-16 10:13:11 +0200

    fixed fold click on player turn

--- Src/Game/Online/ReplayPokerGame/Utils/PlayerUtils.cs
+++ Src/Game/Online/ReplayPokerGame/Utils/PlayerUtils.cs
@@ -1,6 +1,6 @@
 using Microsoft.Playwright;
-using Poker.Online.Events;
 using Poker.Players;
+using Poker.Events;
 
 namespace Poker.Online.Utils;
 
@@ -8,7 +8,10 @@ public static class PlayerUtils {
    
 
     public static async void PlayTurn(object sender, OnPlayerTurnEventArgs e) {
-        var page = e.Page;
+        var page = ReplayPokerGame.Page;
+        if(page == null) {
+            return;
+        }
         var state = e.GameState; 
         var player = state.Player;
         
@@ -26,15 +29,29 @@ public static class PlayerUtils {
         }
 
     }
-
+    
     private static async Task Fold(IPage page) {
-        
+        var button = page.Locator(".BettingControls__action--defensive").First;
+        await button.ClickAsync();
+        var buttonDisappearTask = Task.Run(async () => {
+                var playerLoc = page.Locator(".Seat.Seat--currentUser.Seat--currentPlayer");
+                while(await playerLoc.CountAsync() == 1);
+                }); 
+        var checkDialogBox = Task.Run(async () => {
+                var checkButtonLoc = page.Locator(".Button.check").First;
+                await checkButtonLoc.ClickAsync();
+                });
+        await Task.WhenAny(new Task[] { buttonDisappearTask, checkDialogBox });
     }
     private static async Task Check(IPage page) {
-
+        var button = page.Locator(".BettingControls__action--neutral").First;
+        await button.ClickAsync();
     }
     private static async Task Bet(IPage page, int value) {
-        
+        var button = page.Locator(".BettingControls__action--aggressive").First;
+        var inputLoc = page.Locator("NumberInput > input");
+        await inputLoc.FillAsync(value.ToString());
+        await button.ClickAsync();
     }
 
 }
